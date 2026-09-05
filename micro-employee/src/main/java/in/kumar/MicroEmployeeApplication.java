bed5920930486e73fe8a366469227dd5d7d6c84d
soumen kumar bera <soumenbera4017@gmail.com>
2026-09-05 14:46:53 +0530

    microservices learning

---
+++ micro-employee/src/main/java/in/kumar/MicroEmployeeApplication.java
@@ -0,0 +1,13 @@
+package in.kumar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroEmployeeApplication.class, args);
+	}
+
+}
---
+++ micro-employee/src/test/java/in/kumar/MicroEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package in.kumar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
