01ef160bc5cf856f2d015cc18014d89fe7360613
soumen kumar bera <soumenbera4017@gmail.com>
2026-09-10 17:35:25 +0530

    EmployeeController api and service logic writen

--- micro-employee/src/main/java/in/kumar/controller/EmployeeController.java
+++ micro-employee/src/main/java/in/kumar/controller/EmployeeController.java
@@ -1,14 +1,36 @@
 package in.kumar.controller;
 
+import in.kumar.dto.EmployeeDto;
+import in.kumar.entities.Employee;
+import in.kumar.payload.ApiResponse;
+import in.kumar.services.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/employees")
 public class EmployeeController {
 
+    @Autowired
+    private EmployeeService employeeService;
 
     //save
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<Employee>> saveEmployee(@Valid @RequestBody EmployeeDto employeeDto){
+       ApiResponse<Employee> savedEmployeeResponse = employeeService.saveEmployee(employeeDto);
+       return ResponseEntity.status(HttpStatus.CREATED).body(savedEmployeeResponse);
+
+
+
+    }
     //getAllEmployee
     //getSingleEmployee
 }
--- micro-employee/src/main/java/in/kumar/services/EmployeeServiceImpl.java
+++ micro-employee/src/main/java/in/kumar/services/EmployeeServiceImpl.java
@@ -4,11 +4,14 @@ import in.kumar.dto.EmployeeDto;
 import in.kumar.entities.Employee;
 import in.kumar.payload.ApiResponse;
 import in.kumar.repositories.EmployeeRepo;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.swing.text.html.parser.Entity;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -16,13 +19,21 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Autowired
     private EmployeeRepo employeeRepo;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
 
     @Override
     public ApiResponse<Employee> saveEmployee(EmployeeDto employeeDto) {
 
+      Employee employee =  modelMapper.map(employeeDto, Employee.class); // this work one object to convert another object
 
+        employee.setId(UUID.randomUUID().toString());
+        employee.setDatetime(LocalDateTime.now().toString());
 
-        return null;
+        Employee saveEmployee = employeeRepo.save(employee);
+
+        return new ApiResponse<>("Success", "Employee data create", saveEmployee);
     }
 
     @Override
